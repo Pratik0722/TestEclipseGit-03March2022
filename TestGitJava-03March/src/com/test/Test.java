f83f0e9757312414c3726c654b182efcd2ddf3d6
Pratik0722 <pkdingaria26@gmail.com>
2022-03-04 01:01:43 +0530

    Second Commit

--- TestGitJava-03March/src/com/test/Test.java
+++ TestGitJava-03March/src/com/test/Test.java
@@ -3,6 +3,7 @@ package com.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello we are operating git here");
+		System.out.println("Added few words Hello world");
 	}
 
 }
