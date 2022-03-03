9f036726619fe6f9bd87831f7908cc9a9ad427d1
Pratik0722 <100823553+Pratik0722@users.noreply.github.com>
2022-03-04 01:07:33 +0530

    Test added

--- TestGitJava-03March/src/com/test/Test.java
+++ TestGitJava-03March/src/com/test/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello we are operating git here");
 		System.out.println("Added few words Hello world");
+		System.out.println("Editing from git to eclipse");
 	}
 
 }
